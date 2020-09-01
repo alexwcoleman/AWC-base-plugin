695b48e0dd4b2fdcb91470f4eebb964996b8e994
Alex Coleman <alexanderwinstoncoleman@gmail.com>
2020-09-01 12:03:23 -0700

    Basic Options Page + Submenu

--- admin/class-awc-base-admin.php
+++ admin/class-awc-base-admin.php
@@ -100,4 +100,103 @@ class Awc_Base_Admin {
 
 	}
 
+	/**
+	 * Register the administration menu for this plugin into the WordPress Dashboard menu.
+	 *
+	 * @since    1.0.0
+	 */
+	public function add_plugin_admin_menu() {
+
+		/**
+		 * Add a settings page for this plugin to the Settings menu.
+		 *
+		 * NOTE:  Alternative menu locations are available via WordPress administration menu functions.
+		 *
+		 *        Administration Menus: http://codex.wordpress.org/Administration_Menus
+		 *
+		 * add_options_page( $page_title, $menu_title, $capability, $menu_slug, $function);
+		 *
+		 * @link https://codex.wordpress.org/Function_Reference/add_options_page
+		 */
+
+		add_menu_page( 'PAGE WHAT??????', 'Settings Page Title', 'manage_options', 'partials/' . $this->plugin_name . '-options', [ $this, 'display_plugin_option_page' ], '', 30);
+		add_submenu_page( 'partials/' . $this->plugin_name . '-options', 'Plugin Settings Page Title', 'Submenu Title', 'manage_options', $this->plugin_name, array($this, 'display_plugin_setup_page')
+		);
+
+	}
+
+	 /**
+	 * Add settings action link to the plugins page.
+	 *
+	 * @since    1.0.0
+	 */
+	public function add_action_links( $links ) {
+
+		/*
+		*  Documentation : https://codex.wordpress.org/Plugin_API/Filter_Reference/plugin_action_links_(plugin_file_name)
+		*/
+	$settings_link = array(
+		'<a href="' . admin_url( 'options-general.php?page=' . $this->plugin_name ) . '">' . __( 'Settings', $this->plugin_name ) . '</a>',
+	);
+	return array_merge(  $settings_link, $links );
+
+	}
+
+	/**
+	 * Render the settings page for this plugin.
+	 *
+	 * @since    1.0.0
+	 */
+	public function display_plugin_setup_page() {
+
+		include_once( 'partials/' . $this->plugin_name . '-admin-display.php' );
+
+	}
+
+	/**
+	 * Render the Options page for this plugin.
+	 *
+	 * @since    1.0.0
+	 */
+	public function display_plugin_option_page() {
+
+		include_once( 'partials/' . $this->plugin_name . '-options.php' );
+
+	}
+
+	/**
+	 * Validate fields from admin area plugin settings form ('exopite-lazy-load-xt-admin-display.php')
+	 * @param  mixed $input as field form settings form
+	 * @return mixed as validated fields
+	 */
+	public function validate($input) {
+
+		$valid = array();
+
+		$valid['example_checkbox'] = ( isset( $input['example_checkbox'] ) && ! empty( $input['example_checkbox'] ) ) ? 1 : 0;
+		$valid['example_text'] = ( isset( $input['example_text'] ) && ! empty( $input['example_text'] ) ) ? esc_attr( $input['example_text'] ) : 'default';
+		$valid['example_select'] = ( isset($input['example_select'] ) && ! empty( $input['example_select'] ) ) ? esc_attr($input['example_select']) : 1;
+
+		return $valid;
+
+		// -- OR --
+
+		$options = get_option( $this->plugin_name );
+
+		$options['example_checkbox'] = ( isset( $input['example_checkbox'] ) && ! empty( $input['example_checkbox'] ) ) ? 1 : 0;
+		$options['example_text'] = ( isset( $input['example_text'] ) && ! empty( $input['example_text'] ) ) ? esc_attr( $input['example_text'] ) : 'default';
+		$options['example_select'] = ( isset($input['example_select'] ) && ! empty( $input['example_select'] ) ) ? esc_attr($input['example_select']) : 1;
+
+		return $options;
+
+	}
+
+	public function options_update() {
+
+		register_setting( $this->plugin_name, $this->plugin_name, array(
+		'sanitize_callback' => array( $this, 'validate' ),
+		) );
+
+	}
+
 }
